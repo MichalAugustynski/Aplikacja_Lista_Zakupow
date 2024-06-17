c5d404a39b97bc3b2185afb2d85567d7c210d68d
LukaszWielki <166722960+LukaszWielki@users.noreply.github.com>
2024-06-17 23:32:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import Groceries_list
+import os
+def create_new_list(folder_path,name, length,title):
+	file_path = os.path.join(folder_path,name)
+	with open(file_path,'w') as file:
+		str_punctation = title + "\n" + "---------------------" + "\n"
 
-title = input("Tytuł listy: ")
-folder_path = r"C:\Users
-# Powyżej trzeba dodac ścieżkę na razie zostawiłem to tak
-
-file_name = input("Nazwa listy: ") + ".txt"
-length =int(input("Jak długa ma byc twoja lista?"))
-Groceries_list.create_new_list(folder_path,file_name,length,title)
+		for i in range(1,length+1):
+			str_ = f'{i}. \n'
+			str_punctation += str_
+			# file.write(i,'.')
+		file.write(str_punctation)
+		print("Ścieżka twojej listy: ", file_path)

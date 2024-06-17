e35b9f1ef14fa36b38b8b1ef4deda766e59146a3
LukaszWielki <166722960+LukaszWielki@users.noreply.github.com>
2024-06-17 23:24:31 +0200

    Update main.py gitowa

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #od nowa
+import os
+import shutil
+
+class listy:
+    def __init__(self, tytul):
+        self.tytul = tytul
+        self.nazwa = f"{self.tytul}.txt"
+        self.produkt = []
+        self.wczytaj_produkt()
+
+    def dodaj_produkt(self, produkt):
+        self.produkt.append(produkt)
+        with open(self.nazwa, 'a') as file:
+            file.write(f"{produkt}\n")
+
+    def wczytaj_produkt(self):
+        if os.path.exists(self.nazwa):
+            with open(self.nazwa, 'r') as file:
+                self.produkt = file.readlines()
+                self.produkt = [produkt.strip() for produkt in self.produkt]
+        else:
+            self.produkt = []

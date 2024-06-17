0644918b58fe0576881b84e3cec78975ba45912d
DanielSterzel <Daniel@Sterzel>
2024-06-17 22:09:56 +0200

    Zmiana wyswietlanego napisu

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import Groceries_list
 title = input("Tytuł listy: ")
 # folder_path = r"C:\Users
 # Powyżej trzeba dodac ścieżkę na razie zostawiłem to tak
-folder_path_name = input("Please enter the path you want your list to be saved in")
+folder_path_name = input("Podaj ścieżkę, w której chcesz zapisać swoją listę: ")
 file_name = input("Nazwa listy: ") + ".txt"
 length =int(input("Jak długa ma byc twoja lista?"))
 Groceries_list.create_new_list(folder_path_name,file_name,length,title)

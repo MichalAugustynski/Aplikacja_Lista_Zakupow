bc76919be44a7733501fbab426a7db2f786941f5
LukaszWielki <166722960+LukaszWielki@users.noreply.github.com>
2024-06-17 23:33:26 +0200

    Update Groceries_list.py git

--- Groceries_list.py
+++ Groceries_list.py
@@ -1,15 +1,31 @@
 import os
-def create_new_list(folder_path,name, length,title):
-	file_path = os.path.join(folder_path,name)
-	with open(file_path,'w') as file:
-		str_punctation = title + "\n" + "---------------------" + "\n"
 
-		for i in range(1,length+1):
-			str_ = f'{i}. \n'
-			str_punctation += str_
-			# file.write(i,'.')
-		file.write(str_punctation)
-		print("Ścieżka twojej listy: ", file_path)
+def title_of_list():
+    title_list = input("Give the name the list of items: ")
+
+    folder_path = './lists'  # Możesz zmienić ścieżkę do folderu, gdzie będą przechowywane listy
+    file_path = os.path.join(folder_path, f"{title_list}.txt")
+
+    # 3. Umożliwienie użytkownikowi dodawania przedmiotów do listy i zapisywanie ich do pliku
+    print(f"Creating new lists: {title_list}")
+    print("to finish write 'end'.")
+
+    with open(file_path, 'w') as file:
+        file.write(f"Lista zakupów: {title_list}\n")
+        file.write("-----------------\n")
+
+        while True:
+            item = input("Dodaj przedmiot do listy zakupów: ")
+            if item.lower() == 'end':
+                break
+            file.write(f"- {item}\n")
+
+    print(f"Lista zakupów '{title_list}' została zapisana do pliku: {file_path}")
+
+
+title_of_list()
+def edit_list():
+
 
 
 

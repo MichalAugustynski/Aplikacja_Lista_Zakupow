905130e705a13bd956dfd1e28e07472729a9dea8
DanielSterzel <Daniel@Sterzel>
2024-06-17 22:07:57 +0200

    Rozwiazanie problemu

--- folder_path.py
+++
@@ -1,3 +0,0 @@
-import Groceries_list
-
-folder_path = input("Please enter the path you would like your file to be saved in: ")
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import Groceries_list
 
-title = input("Tytuł listy: ")
-folder_path = r"C:\Users
-# Powyżej trzeba dodac ścieżkę na razie zostawiłem to tak
 
+title = input("Tytuł listy: ")
+# folder_path = r"C:\Users
+# Powyżej trzeba dodac ścieżkę na razie zostawiłem to tak
+folder_path_name = input("Please enter the path you want your list to be saved in")
 file_name = input("Nazwa listy: ") + ".txt"
 length =int(input("Jak długa ma byc twoja lista?"))
-Groceries_list.create_new_list(folder_path,file_name,length,title)
+Groceries_list.create_new_list(folder_path_name,file_name,length,title)
